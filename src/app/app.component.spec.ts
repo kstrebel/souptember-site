c21649801fe4f070f7716ea4af4214695e33e271
Kathy Strebel <kthstrebel4@gmail.com>
2023-09-02 21:34:03 -0500

    Fix issues with angular

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,33 +1,34 @@
-import { TestBed } from "@angular/core/testing";
-import { RouterTestingModule } from "@angular/router/testing";
-import { AppComponent } from "./app.component";
-import { NO_ERRORS_SCHEMA } from "@angular/core";
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { AppComponent } from './app.component';
 
 describe("AppComponent", () => {
-    beforeEach(async () => {
-        await TestBed.configureTestingModule({
-            imports: [RouterTestingModule],
-            declarations: [AppComponent],
-            schemas: [NO_ERRORS_SCHEMA],
-        }).compileComponents();
-    });
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [RouterTestingModule],
+			declarations: [AppComponent],
+			schemas: [NO_ERRORS_SCHEMA],
+		}).compileComponents();
+	});
 
-    it("should create the app", () => {
-        const fixture = TestBed.createComponent(AppComponent);
-        const app = fixture.componentInstance;
-        expect(app).toBeTruthy();
-    });
+	it("should create the app", () => {
+		const fixture = TestBed.createComponent(AppComponent);
+		const app = fixture.componentInstance;
+		expect(app).toBeTruthy();
+	});
 
-    it("should have as title 'souptember-site'", () => {
-        const fixture = TestBed.createComponent(AppComponent);
-        const app = fixture.componentInstance;
-        expect(app.title).toEqual("souptember-site");
-    });
+	it("should have as title 'souptember-site'", () => {
+		const fixture = TestBed.createComponent(AppComponent);
+		const app = fixture.componentInstance;
+		expect(app.title).toEqual("souptember-site");
+	});
 
-    it("should render title", () => {
-        const fixture = TestBed.createComponent(AppComponent);
-        fixture.detectChanges();
-        const compiled = fixture.nativeElement as HTMLElement;
-        expect(compiled.querySelector(".content span")?.textContent).not.toBeNull();
-    });
+	it("should render title", () => {
+		const fixture = TestBed.createComponent(AppComponent);
+		fixture.detectChanges();
+		const compiled = fixture.nativeElement as HTMLElement;
+		expect(compiled.querySelector(".content span")?.textContent).not.toBeNull();
+	});
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from "@angular/core";
+import { MatIconModule } from "@angular/material/icon";
+import { MatToolbarModule } from "@angular/material/toolbar";
 import { BrowserModule } from "@angular/platform-browser";
-
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { RouterModule } from "@angular/router";
+
 import { AboutComponent } from "./about/about.component";
 import { AppComponent } from "./app.component";
 import { CountdownComponent } from "./countdown/countdown.component";
 import { HomeComponent } from "./home/home.component";
 import { InvolvementComponent } from "./involvement/involvement.component";
-import { MatToolbarModule } from "@angular/material/toolbar";
-import { MatIconModule } from "@angular/material/icon";
 
 @NgModule({
 	declarations: [
